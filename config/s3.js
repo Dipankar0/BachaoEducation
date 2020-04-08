71449f2b310c17c35ea659483ba964ad894b5603
Dipankar Bhadra <dipankarbhadra@Dipankars-MacBook-Pro.local>
2020-04-08 12:35:28 +0600

    This is not permanent solution

---
+++ config/s3.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const connect = {
+  accessKeyId: 'AKIASHTD4GJXJAJ3JRRA',
+  secretAccessKey: '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',
+  Bucket: 'educationbachaobucket'
+};
+
+module.exports = connect;
