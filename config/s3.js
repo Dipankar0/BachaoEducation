d9a841207b0551d3f634cc0c3fe857f076679e18
Dipankar Bhadra <dipankarbhadra@Dipankars-MacBook-Pro.local>
2020-05-16 20:17:45 +0600

    removed all files from config

--- config/s3.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const connect = {
-  accessKeyId: 'AKIASHTD4GJXJAJ3JRRA',
-  secretAccessKey: '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',
-  Bucket: 'educationbachaobucket'
-};
-
-module.exports = connect;
